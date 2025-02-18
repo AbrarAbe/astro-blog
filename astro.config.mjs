ab3cd2350ecceb03be640a19519ef4600a63a1a7
AbrarAbe <abrar.abe01@gmail.com>
2025-02-18 13:01:15 +0700

    Add rss and island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://stellular-crumble-76cc2d.netlify.app",
+  integrations: [preact()]
+});
